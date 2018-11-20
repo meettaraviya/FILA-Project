d57aaf2f7f3a3f8329ade12c495ab84668305a1f
Meet Taraviya <meet11061997@gmail.com>
2018-11-21 00:15:38 +0530

    A

--- nim_agents.py
+++ nim_agents.py
@@ -3,10 +3,14 @@ from keras import backend as K
 from keras.utils.generic_utils import get_custom_objects
 from keras.layers import Activation, Dense, Maximum
 import keras
+import  itertools
 
 import operator
 import numpy as np
 from games import *
+
+n_rows = 4
+limit = 4
 
 def NimsGameRandomMove(board):
 
@@ -14,6 +18,31 @@ def NimsGameRandomMove(board):
 	move[0] = np.random.choice(np.where(board>0)[0])
 	move[1] = np.random.randint(1, 1+board[move[0]])
 	return move
+
+
+class EpsilonRandomNimAgent:
+	def __init__(self, epsilon):
+		self.epsilon = epsilon
+	def get_move(self, board):
+
+		move = [None, None]
+		if np.random.binomial(1, self.epsilon) == 1:
+			return NimsGameRandomMove(board)
+
+		else:
+			xor = np.bitwise_xor.reduce(board)
+			if xor != 0:
+				move = [None, None]
+				move[0] = np.where(np.bitwise_xor(board, xor)<board)[0][0]
+				move[1] = board[move[0]] - (board[move[0]]^xor)
+				return move
+
+			else:
+				return NimsGameRandomMove(board)
+	
+	def gameOver(self,win):
+		pass
+
 
 class OptimalNimAgent:
 
@@ -31,22 +60,19 @@ class OptimalNimAgent:
 	def gameOver(self,win):
 		pass
 
-def NN(n_rows):
+def NN(n_rows, limit):
 	get_custom_objects().update({'sine': Activation(K.sin)})
-
 	model = Sequential()
-	model.add(Dense(units=n_rows, activation='sigmoid', input_dim=n_rows))
-	model.add(Dense(units=n_rows, activation='sine'))
+	model.add(Dense(units=(limit+1)*n_rows, activation='sine', input_dim=n_rows))
+	model.add(Dense(units=limit+1, activation='sine'))
 	model.add(Dense(units=1, activation='sigmoid'))
-	# model.compile(loss=keras.losses.categorical_crossentropy,
-	# sgd =keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 
 def getNimActions(board):
 	actions = []
 	for i in range(len(board)):
-		for j in range(board[i]+1):
+		for j in range(1, board[i]+1):
 			actions.append((i,j))
 	return actions
 
@@ -61,22 +87,23 @@ def getNextState(board,actions):
 
 class BatchMCNNNimAgent:
 
-	def __init__(self, batch_size=100, n_rows=10):
+	def __init__(self, batch_size=100, n_rows=10, limit=5):
 		self.steps_since_update = 0
 		self.batch_size = batch_size
 		self.n_rows	= n_rows
-		self.model = NN(self.n_rows)
+		self.limit = limit
+		self.model = NN(self.n_rows, self.limit)
 		self.time_since_train = 0
 		self.history = [[]]
 		self.completed_runs = 0
 
 	def train(self):
-		x_train = [h[:-1] for h in self.history[:completed_runs]]
-		y_train = [ [h[-1]]*(len(h)-1) for h in self.history[:completed_runs]]
+		x_train = [h[:-1] for h in self.history[:self.completed_runs]]
+		y_train = [ [h[-1]]*(len(h)-1) for h in self.history[:self.completed_runs]]
 		x_train = np.array(list(itertools.chain.from_iterable(x_train)),dtype= int)
 		y_train = np.array(list(itertools.chain.from_iterable(y_train)),dtype= int)
 
-		self.model.fit(x_train,y_train,epochs=10)
+		self.model.fit(x_train,y_train, epochs=10)
 
 	def getBestNextStateIdx(self,nextstates):
 		if self.time_since_train == 100 and self.completed_runs>0:
@@ -105,19 +132,32 @@ class BatchMCNNNimAgent:
 		self.history.append([])
 
 if __name__ == "__main__":
-	init_board = np.array([8,3,2,4,1,12], dtype=int)
-	print(init_board)
-	game = NimsGame(init_board)
-	agents = [OptimalNimAgent(), BatchMCNNNimAgent(n_rows=len(init_board))]
+	# print(init_board)
+	np.random.seed(1)
+	agents = [BatchMCNNNimAgent(n_rows=n_rows), OptimalNimAgent()]
+	# agents = [OptimalNimAgent(),OptimalNimAgent()]
+	wins = 0
+	games = [NimsGame(np.random.randint(2**limit, size=n_rows).copy()) for i in range(1000)]
 
-	while game.get_winner() is None:
-		game.print_game()
-		move = agents[game.get_player()].get_move(game.get_board())
-		game.play_move(move)
+	winner = [np.bitwise_xor.reduce(games[i].board) == 0 for i in range(1000)]
+	print(sum(winner))
+	
+	for _ in range(1000000):
+		if (_)%1000 == 999:
+			# agents[0] = EpsilonRandomNimAgent(1)
+			print("Wins=", wins)
+			wins = 0
+		game = games[_%1000]
+		# print(_)
+
+		while game.get_winner() is None:
+			# game.print_game()
+			move = agents[game.get_player()].get_move(game.get_board())
+			game.play_move(move)
 		# print(move)
 
-	agents[0].gameOver(1-game.get_winner())
-	agents[1].gameOver(game.get_winner())
+		agents[0].gameOver(1-game.get_winner())
+		agents[1].gameOver(game.get_winner())
+		wins += game.get_winner()
 
-	# print(game.game_state)
-	print(["Player 1 wins!", "Player 2 wins!"][game.get_winner()])
+	print("Total Wins=", sum(winner))
